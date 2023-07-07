10e0c8e61a68f1bd33a77d68b2f9a433ffe1a8fc
mahdirahmani1376 <rahmanimahdi20@yahoo.com>
2023-07-07 14:04:47 +0330

    second commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+
+            </h1>
+            <div>
+
+            </div>
+        </article>
+    @endforeach
+@endsection
